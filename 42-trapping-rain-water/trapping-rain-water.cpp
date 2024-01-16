cb580e9d73f69fa47bb09164ede1faddb91dfd59
RohitKotha <119722685+RohitKotha@users.noreply.github.com>
2024-01-16 10:17:50 +0530

    Time: 18 ms (18.04%) | Memory: 20.3 MB (43.50%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-     vector<long long> left,right;
-        long long max = 0;
-        for(long long i=0 ; i<height.size() ; i++){
-            if(height[i]>=max){
-                max=height[i];
+        int n = height.size();
+        int output = 0;
+        
+        if (n <= 2) return 0;
+        
+        int left = 0;
+        int right = n - 1;
+        int left_max = 0;
+        int right_max = 0;
+        
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] > left_max) {
+                    left_max = height[left];
+                } else {
+                    output += left_max - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] > right_max) {
+                    right_max = height[right];
+                } else {
+                    output += right_max - height[right];
+                }
+                right--;
             }
-            left.push_back(max);
         }
-        max = 0;
-        for(long long i=height.size()-1 ; i>=0 ; i--){
-            if(height[i]>=max){
-                max=height[i];
-            }
-            right.push_back(max);
-        }
-        reverse(right.begin(),right.end());
-        long long sum=0;
-        for(long long i=0 ; i<height.size() ; i++){
-            sum+=(min(left[i],right[i])-height[i]);
-        }
-        return sum;   
+        
+        return output;
     }
 };
