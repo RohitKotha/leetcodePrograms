77aeff55c3ada9e5a404dbb30cd5340c0969509c
RohitK <119722685+RohitKotha@users.noreply.github.com>
2024-02-23 21:44:09 +0530

    Time: 734 ms (74.96%) | Memory: 27.1 MB (97.23%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -5,9 +5,11 @@ class Solution:
         l2 = []
         min1=l1[0]
         max1=0
+        max2=0
         for i in l1[1:]:
             if i<min1:
                 min1=i
+               
             else:
                 max1=max(max1,i-min1)
-        return max1
+        return max1+max2
