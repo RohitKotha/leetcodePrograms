930bd188ab0391dc365a4e8754ba18b2c4d2732c
RohitK <119722685+RohitKotha@users.noreply.github.com>
2024-07-15 22:34:10 +0530

    Time: 1491 ms (5.03%) | Memory: 329.2 MB (7.21%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,90 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+TreeNode* fun(int r, map<int,vector<int>>& mp) {
+    if (mp.find(r) == mp.end()) {
+      return NULL;
+    }
+    TreeNode* root = new TreeNode(r);
+    int left = mp[r][0];
+    int right = mp[r][1];
+    if (left != 0) {
+      root->left = fun(left, mp);
+    }
+    if (right != 0) {
+      root->right = fun(right, mp);
+    }
+    return root;
+  }
+
+    TreeNode* createBinaryTree(vector<vector<int>>& desc) {
+          map<int,vector<int>>mp;
+        map<int,int>ro;
+        int n=desc.size();
+        for(int i=0;i<n;i++){
+        int p = desc[i][0];
+        int c = desc[i][1];
+        int l = desc[i][2];
+
+        
+            if(ro.find(p)==ro.end()){
+                ro[p]=1;
+            }
+            ro[c]+=2;
+        
+            if(mp.find(p)!=mp.end()){
+                if(l==1){
+                    mp[p][0]=c;
+                }
+                else if(l==0){
+                    mp[p][1]=c;
+                }
+            }else{
+                mp[p]=vector<int>(2,0);
+                if(l==1){
+                    mp[p][0]=c;
+                }
+                else if(l==0){
+                    mp[p][1]=c;
+                }
+            }
+ if(mp.find(c)==mp.end()){
+                mp[c]=vector<int>(2,0);
+            }
+        }
+
+
+/////////////////////////////////////////////////////////////////
+for (const auto& pair : mp) {
+        cout << "Key: " << pair.first << ", Values: ";
+        for (const auto& val : pair.second) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+/////////////////////////////////////////////////////////////////
+int pp;
+for(auto it:ro){
+    if(it.second==1){
+pp=it.first;
+break;
+    }
+}
+cout<<pp;
+
+TreeNode* root=fun(pp,mp);
+
+
+        return root;
+    }
+};
