cb357937fab54571ed016ec3cf2d9194993db0f4
RohitK <119722685+RohitKotha@users.noreply.github.com>
2025-01-07 11:23:59 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (29.52%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+       sort(words.begin(), words.end(), [](const string& a, const string& b) {
+            return a.size() < b.size();
+        });
+        vector<string>ans;
+        for(int i=0;i<words.size();i++){
+            for(int j=i+1;j<words.size();j++){
+                if(words[j].find(words[i])!=string::npos){
+                    ans.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
